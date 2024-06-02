63790a63a90f8e2fa00a3866191ea2ffe7f7d3fa
Aswin P T <aswinpt.apt@gmail.com>
2024-06-02 17:08:07 +0530

    updated reels

--- public/js/reels.js
+++ public/js/reels.js
@@ -1,54 +1,86 @@
 const reelsVideos = document.getElementById("reelsVideos");
 
-const videosData = [
-  {
-    videoSrc: "../assets/reels/videos/1.mp4",
-    likes: "100k",
-    comments: "11.1k",
-    username: "Username",
-    userImg: "../assets/reels/images/user.jpg",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, eos.",
-    song: "Song name . Original song",
-  },
-  {
-    videoSrc: "../assets/reels/videos/2.mp4",
-    likes: "100k",
-    comments: "11.1k",
-    username: "Username 2",
-    userImg: "../assets/reels/images/user.jpg",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, eos.",
-    song: "Song name . Original song",
-  },
-  {
-    videoSrc: "../assets/reels/videos/3.mp4",
-    likes: "100k",
-    comments: "11.1k",
-    username: "Username 3",
-    userImg: "../assets/reels/images/user.jpg",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, eos.",
-    song: "Song name . Original song",
-  },
-  {
-    videoSrc: "../assets/reels/videos/4.mp4",
-    likes: "100k",
-    comments: "11.1k",
-    username: "Username 4",
-    userImg: "../assets/reels/images/user.jpg",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, eos.",
-    song: "Song name . Original song",
-  },
-];
+import {fetchReels, fetchUserById} from '../js/apiFetch.js'
+
+async function receiveReels() {
+  const data = await fetchReels();
+  const jsonData = [];
+  for (const element of data) {
+    const {
+      id,
+      userId,
+      videoSrc,
+      likes,
+      comments,
+      description,
+      song
+    } = element;
+    const user = await fetchUserById(userId);
+    const dataObject = {
+      id,
+      userId,
+      videoSrc,
+      likes,
+      comments,
+      description,
+      song,
+      user,
+    };
+    jsonData.push(dataObject);
+  }
+  return jsonData;
+}
+// const videosData = [
+//   {
+//     videoSrc: "../assets/reels/videos/1.mp4",
+//     likes: "100k",
+//     comments: "11.1k",
+//     username: "Username",
+//     userImg: "../assets/reels/images/user.jpg",
+//     description:
+//       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, eos.",
+//     song: "Song name . Original song",
+//   },
+//   {
+//     videoSrc: "../assets/reels/videos/2.mp4",
+//     likes: "100k",
+//     comments: "11.1k",
+//     username: "Username 2",
+//     userImg: "../assets/reels/images/user.jpg",
+//     description:
+//       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, eos.",
+//     song: "Song name . Original song",
+//   },
+//   {
+//     videoSrc: "../assets/reels/videos/3.mp4",
+//     likes: "100k",
+//     comments: "11.1k",
+//     username: "Username 3",
+//     userImg: "../assets/reels/images/user.jpg",
+//     description:
+//       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, eos.",
+//     song: "Song name . Original song",
+//   },
+//   {
+//     videoSrc: "../assets/reels/videos/4.mp4",
+//     likes: "100k",
+//     comments: "11.1k",
+//     username: "Username 4",
+//     userImg: "../assets/reels/images/user.jpg",
+//     description:
+//       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, eos.",
+//     song: "Song name . Original song",
+//   },
+// ];
+
+const videosData = await receiveReels();
 
 function createVideoElement(data) {
   const videoElement = document.createElement("div");
   videoElement.classList.add("video");
 
   videoElement.innerHTML = `
-    <video src="${data.videoSrc}"></video>
+    <iframe src="${data.videoSrc}"></iframe>
     <button class="top">
     <svg xmlns="http://www.w3.org/2000/svg" width="17px" height="17px" viewBox="0 0 50 50">
   <path clip-rule="evenodd" d="M1.5 13.3c-.8 0-1.5.7-1.5 1.5v18.4c0 .8.7 1.5 1.5 1.5h8.7l12.9 12.9c.9.9 2.5.3 2.5-1v-9.8c0-.4-.2-.8-.4-1.1l-22-22c-.3-.3-.7-.4-1.1-.4h-.6zm46.8 31.4-5.5-5.5C44.9 36.6 48 31.4 48 24c0-11.4-7.2-17.4-7.2-17.4-.6-.6-1.6-.6-2.2 0L37.2 8c-.6.6-.6 1.6 0 2.2 0 0 5.7 5 5.7 13.8 0 5.4-2.1 9.3-3.8 11.6L35.5 32c1.1-1.7 2.3-4.4 2.3-8 0-6.8-4.1-10.3-4.1-10.3-.6-.6-1.6-.6-2.2 0l-1.4 1.4c-.6.6-.6 1.6 0 2.2 0 0 2.6 2 2.6 6.7 0 1.8-.4 3.2-.9 4.3L25.5 22V1.4c0-1.3-1.6-1.9-2.5-1L13.5 10 3.3-.3c-.6-.6-1.5-.6-2.1 0L-.2 1.1c-.6.6-.6 1.5 0 2.1L4 7.6l26.8 26.8 13.9 13.9c.6.6 1.5.6 2.1 0l1.4-1.4c.7-.6.7-1.6.1-2.2z" fill="white" fill-rule="evenodd"></path>
@@ -85,12 +117,12 @@ function createVideoElement(data) {
         <circle cx="18" cy="12" r="1.5"></circle>
       </svg>
       </button>
-      <img src="${data.userImg}" alt="User profile image" />
+      <img src="${data.user.profilePicture}" alt="User profile image" />
     </div>
     <div class="user_profile">
       <div>
-        <img src="${data.userImg}" alt="User profile image" />
-        <h4><button>${data.username}&nbsp;&nbsp;•</button></h4>
+        <img src="${data.user.profilePicture}" alt="User profile image" />
+        <h4><button>${data.user.username}&nbsp;&nbsp;•</button></h4>
         <button class="follow">Follow</button>
       </div>
       <p>${data.description}•</p>
